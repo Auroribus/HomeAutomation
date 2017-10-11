7f3f78b60c861dfdc227c1e66c462382f2a81738
Steven <stief_27@hotmail.com>
2017-10-11 17:04:57 +0200

    pushing pi script to github

--- doc/Product/README.md
+++
@@ -1,4 +0,0 @@
-# Internet of Things - Home Automation
-
-### Doel van het product
-Home automation en monitoring door middel van Speech to Text technologie
---
+++ src/Raspberry Pi/hotword.py
@@ -0,0 +1,72 @@
+from __future__ import print_function
+import RPi.GPIO as GPIO
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(17,GPIO.OUT)
+GPIO.output(17,0)
+
+import argparse
+import os.path
+import json
+
+import google.oauth2.credentials
+
+from google.assistant.library import Assistant
+from google.assistant.library.event import EventType
+from google.assistant.library.file_helpers import existing_file
+
+
+def process_event(event):
+    """Pretty prints events.
+
+    Prints all events that occur with two spaces between each new
+    conversation and a single space between turns of a conversation.
+
+    Args:
+        event(event.Event): The current event to process.
+    """
+    if event.type == EventType.ON_CONVERSATION_TURN_STARTED:
+        print()
+
+    print(event)
+    
+    if event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED:
+        print(event.args['text'])
+        if event.args['text'] == 'lights on' :
+            print("light on")
+            GPIO.output(17,1)
+        if event.args['text'] == 'lights off':
+            print("light off")
+            GPIO.output(17,0)        
+    
+    if (event.type == EventType.ON_CONVERSATION_TURN_FINISHED and
+            event.args and not event.args['with_follow_on_turn']):
+        print()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('--credentials', type=existing_file,
+                        metavar='OAUTH2_CREDENTIALS_FILE',
+                        default=os.path.join(
+                            os.path.expanduser('~/.config'),
+                            'google-oauthlib-tool',
+                            'credentials.json'
+                        ),
+                        help='Path to store and read OAuth2 credentials')
+    args = parser.parse_args()
+    with open(args.credentials, 'r') as f:
+        credentials = google.oauth2.credentials.Credentials(token=None,
+                                                            **json.load(f))
+
+    with Assistant(credentials) as assistant:
+        for event in assistant.start():
+            process_event(event)
+
+
+if __name__ == '__main__':
+    main()
